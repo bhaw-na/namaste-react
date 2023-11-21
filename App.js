104b220b4b94a944ba508c3fc0be0557fec8f032
Bhawna Pandey <bhawnapandey436@gmail.com>
2023-11-21 19:09:55 +0530

    episode 4

--- App.js
+++ App.js
@@ -1,28 +1,353 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const spanElement = <span>React Functional Components</span>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://i.pinimg.com/736x/26/b4/de/26b4ded0e3d9431dbe120c8026e0b8d8.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const heading = (
-  <h1 id="heading" className="head">
-    Hello world and {spanElement}!
-  </h1>
-);
+const resList = [
+  {
+    info: {
+      id: "769001",
+      name: "THE VIBE-X RESTRO-CAFE",
+      city: "10",
+      slugs: {
+        restaurant: "the-vibe-x-restro-cafe-chandkheda-chandkheda-2",
+      },
+      cloudinaryImageId: "70f10c3eb5479b02648d956b394d6d4f",
+      locality: "Tapovan Circle",
+      areaName: "Chandkheda",
+      costForTwo: "₹200 for two",
+      cuisines: ["Indian"],
+      feeDetails: {
+        restaurantId: "769001",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 3700,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 3700,
+        title: "Delivery Charge",
+        amount: "3700",
+      },
+      promoted: true,
+      adTrackingId:
+        "cid=9318045~p=1~eid=0000018b-f1e1-04d3-35da-418100d3017d~srvts=1700570137811~56861",
+      sla: {
+        deliveryTime: 27,
+        minDeliveryTime: 27,
+        maxDeliveryTime: 27,
+        serviceability: "SERVICEABLE",
+        slaString: "27 MINS",
+        lastMileTravelString: "4.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-21 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+      },
+    },
+    analytics: {},
+    cta: {},
+  },
+  {
+    info: {
+      id: "528154",
+      name: "Biryani Hub",
+      city: "10",
+      slugs: {
+        restaurant: "biryani-hub-navrangpura-navrangpura",
+      },
+      cloudinaryImageId: "vvn1ebwjrotckljlxvnm",
+      locality: "ashram road subhash bridge",
+      areaName: "Shahibhag",
+      costForTwo: "₹100 for two",
+      cuisines: ["Biryani", "Beverages"],
+      avgRating: 3.5,
+      feeDetails: {
+        restaurantId: "528154",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 13900,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 13900,
+        title: "Delivery Charge",
+        amount: "13900",
+      },
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 49,
+        minDeliveryTime: 49,
+        maxDeliveryTime: 49,
+        serviceability: "SERVICEABLE",
+        slaString: "49 MINS",
+        lastMileTravelString: "13.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-22 00:00:00",
+        opened: true,
+      },
+      aggregatedDiscountInfo: {},
+      badges: {},
+      aggregatedDiscountInfoV2: {},
+    },
+    analytics: {},
+    cta: {},
+  },
+  {
+    info: {
+      id: "705524",
+      name: "Stoneed Munchies",
+      city: "10",
+      slugs: {
+        restaurant: "stoneed-munchies-gandhinagar-gandhinagar",
+      },
+      cloudinaryImageId: "8004b5261ba40b4fc32d799ca4de5732",
+      locality: "Pramukh Mastana Arcade",
+      areaName: "Gandhinagar",
+      costForTwo: "₹300 for two",
+      cuisines: ["North Indian"],
+      avgRating: 3.6,
+      feeDetails: {
+        restaurantId: "705524",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 5900,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5900,
+        title: "Delivery Charge",
+        amount: "5900",
+      },
+      totalRatingsString: "50+",
+      promoted: true,
+      adTrackingId:
+        "cid=9512570~p=2~eid=0000018b-f1e1-04d3-35da-418200d30205~srvts=1700570137811~56861",
+      sla: {
+        deliveryTime: 36,
+        minDeliveryTime: 36,
+        maxDeliveryTime: 36,
+        serviceability: "SERVICEABLE",
+        slaString: "36 MINS",
+        lastMileTravelString: "7.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-22 03:00:00",
+        opened: true,
+      },
+      badges: {},
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹999",
+        discountTag: "FLAT DEAL",
+      },
+    },
+    analytics: {},
+    cta: {},
+  },
+  {
+    info: {
+      id: "676549",
+      name: "Muskaan'S Kitchen",
+      city: "10",
+      slugs: {
+        restaurant: "muskaan's-kitchen-chandkheda-chandkheda",
+      },
+      cloudinaryImageId: "633f229a1747553e15480cdd844d027d",
+      locality: "Olive Residency",
+      areaName: "Chandkheda",
+      costForTwo: "₹599 for two",
+      cuisines: ["North Indian"],
+      avgRating: 4.4,
+      feeDetails: {
+        restaurantId: "676549",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 3700,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 3700,
+        title: "Delivery Charge",
+        amount: "3700",
+      },
+      totalRatingsString: "7",
+      sla: {
+        deliveryTime: 34,
+        minDeliveryTime: 34,
+        maxDeliveryTime: 34,
+        serviceability: "SERVICEABLE",
+        slaString: "34 MINS",
+        lastMileTravelString: "4.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-21 23:30:00",
+        opened: true,
+      },
+      aggregatedDiscountInfo: {},
+      badges: {},
+      aggregatedDiscountInfoV2: {},
+    },
+    analytics: {},
+    cta: {},
+  },
+  {
+    info: {
+      id: "591770",
+      name: "Kwality Walls Frozen Dessert and Ice Cream Shop",
+      city: "10",
+      slugs: {
+        restaurant:
+          "kwality-walls-frozen-dessert-and-ice-cream-shop-chandkheda-chandkheda-4",
+      },
+      cloudinaryImageId: "i3sa1wapjwymmj6zpl2v",
+      locality: "Opp Satyamev Hospital",
+      areaName: "Chandkheda",
+      costForTwo: "₹200 for two",
+      cuisines: ["Desserts", "Ice Cream", "Ice Cream Cakes"],
+      avgRating: 4.6,
+      feeDetails: {
+        restaurantId: "591770",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 3700,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 3700,
+        title: "Delivery Charge",
+        amount: "3700",
+      },
+      totalRatingsString: "100+",
+      promoted: true,
+      adTrackingId:
+        "cid=9475618~p=3~eid=0000018b-f1e1-04d3-35da-418300d30326~srvts=1700570137811~56861",
+      sla: {
+        deliveryTime: 20,
+        minDeliveryTime: 20,
+        maxDeliveryTime: 20,
+        serviceability: "SERVICEABLE",
+        slaString: "20 MINS",
+        lastMileTravelString: "4.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-22 00:00:00",
+        opened: true,
+      },
+      aggregatedDiscountInfo: {},
+      badges: {},
+      aggregatedDiscountInfoV2: {},
+    },
+    analytics: {},
+    cta: {},
+  },
+];
 
-const Title = () => <div id="title">{heading}</div>;
+const ResCard = (props) => {
+  const { resInfo } = props;
 
-// React Component
-const Heading = () => (
-  <div id="container">
-    {Title()}
-    <Title />
-    <Title></Title>
-    <h1 className="heading">React Functional Components!</h1>
-  </div>
-);
+  const { name, cuisines, avgRating, costForTwo, cloudinaryImageId } =
+    resInfo?.info;
+
+  return (
+    <div className="res-card">
+      <img
+        className="card-image"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} Stars</h4>
+      <h4>{costForTwo}</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {resList.map((res, index) => (
+          <ResCard key={res.info.id} resInfo={res} />
+          // No key(not recommended) << index as key << unique id as key(recommended)
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-root.render(<Heading />);
+root.render(<AppLayout />);
